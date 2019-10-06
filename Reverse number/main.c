d3c3374d38a89e14bc4e6a317e98089e6a46606c
Sneha Koshti <sneha.k@sankeysolutions.com>
2019-10-06 21:01:43 +0530

    Reverse of entered number

--- Reverse number/main.c
+++ Reverse number/main.c
@@ -4,17 +4,14 @@ int main()
 {
    int n, reverse = 0;
 
-   printf("Enter a number to reverse\n");
+   printf("Enter a number to reverse: \n");
    scanf("%d", &n);
-
    while (n != 0)
    {
       reverse = reverse * 10;
-      reverse = reverse + n%10;
-      n       = n/10;
+      reverse = reverse + (n%10);
+      n = n/10;
    }
-
    printf("Reverse of entered number is = %d\n", reverse);
-
    return 0;
 }
